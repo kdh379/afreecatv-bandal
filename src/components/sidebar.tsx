d2b7af01adc0f84efd626ece805387fce9e5ac04
kdh379 <ehgus37625@naver.com>
2024-09-21 22:50:37 +0900

    feat: sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,70 +1,138 @@
+import React from "react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useFetch } from "@/hooks/useFetch";
 import { splitAndSortFavorites } from "@/utils/favorites";
+import { Icons } from "@/components/ui/icons";
 
-function LiveBJList({ items }: { items: FavoriteItem[] }) {
-  return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.user_id}>
-          <a
-            href={item.broad_info[0].url}
-            className="flex items-center gap-x-3"
-          >
-            <img
-              src={`https://stimg.afreecatv.com/LOGO/${item.user_id.slice(0, 2)}/${item.user_id}/m/${item.user_id}.webp`}
-              width={25}
-              height={25}
-              className="size-10 rounded-full"
-            />
-            <div>
-              <p className="text-xl font-semibold">{item.user_nick}</p>
-              <p className="line-clamp-1 text-lg">
-                {item.broad_info[0].category_tags.map(String).join(" / ")}
-              </p>
-            </div>
-            <div className="ml-auto flex items-center gap-x-2 before:inline-block before:size-3 before:rounded-full before:bg-red-500">
-              <span aria-label="시청자 수">
-                {item.broad_info[0].total_view_cnt}
-              </span>
-            </div>
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
-}
+const BjImage = ({ userId }: { userId: FavoriteItem["user_id"] }) => (
+  <img
+    src={`https://stimg.afreecatv.com/LOGO/${userId.slice(0, 2)}/${userId}/m/${userId}.webp`}
+    width={25}
+    height={25}
+    className="size-10 rounded-full"
+    alt={`${userId} profile`}
+  />
+);
 
-function OfflineBJList({ items }: { items: FavoriteItem[] }) {
-  return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.user_id}>
-          <div>{item.user_nick}</div>
-        </li>
-      ))}
-    </ul>
-  );
-}
+const BroadPopover = ({ broadInfo }: { broadInfo: BroadInfo }) => (
+  <a href={broadInfo.url}>
+    <img
+      src={broadInfo.broad_img}
+      width={200}
+      height={200}
+      className="rounded-md"
+      alt={`${broadInfo.broad_title} thumbnail`}
+    />
+    <p className="max-w-[200px] whitespace-pre-wrap text-xl">
+      {broadInfo.broad_title}
+    </p>
+  </a>
+);
+
+const BjListItem = ({
+  children,
+  url,
+  isOffline = false,
+}: {
+  children: React.ReactNode;
+  url: string;
+  isOffline?: boolean;
+}) => (
+  <li>
+    <a
+      href={url}
+      className={`flex w-full items-center gap-x-2 rounded-md p-2 text-left transition-colors hover:bg-muted hover:no-underline ${isOffline ? "grayscale" : ""}`}
+    >
+      {children}
+    </a>
+  </li>
+);
+
+const LiveItem = React.memo(({ item }: { item: FavoriteItem }) => (
+  <Tooltip delayDuration={0}>
+    <TooltipTrigger className="w-full">
+      <BjListItem url={item.broad_info[0].url}>
+        <BjImage userId={item.user_id} />
+        <div className="grow overflow-hidden">
+          <h3 className="truncate text-xl font-semibold leading-none">
+            {item.user_nick}
+          </h3>
+          <h4 className="truncate text-lg font-medium">
+            {item.broad_info[0].broad_title}
+          </h4>
+          <p className="line-clamp-1 text-lg text-muted-foreground">
+            {item.broad_info[0].category_tags.join(" / ")}
+          </p>
+        </div>
+        <div className="ml-auto flex items-center gap-x-2 text-lg before:inline-block before:size-2 before:rounded-full before:bg-destructive">
+          <span aria-label="시청자 수">
+            {item.broad_info[0].total_view_cnt}
+          </span>
+        </div>
+      </BjListItem>
+    </TooltipTrigger>
+    <TooltipContent side="right">
+      <BroadPopover broadInfo={item.broad_info[0]} />
+    </TooltipContent>
+  </Tooltip>
+));
+
+LiveItem.displayName = "LiveItem";
+
+const OfflineItem = React.memo(({ item }: { item: FavoriteItem }) => (
+  <BjListItem url={`https://bj.afreecatv.com/${item.user_id}`} isOffline>
+    <BjImage userId={item.user_id} />
+    <div className="overflow-hidden">
+      <h3 className="truncate text-xl font-semibold leading-none">
+        {item.user_nick}
+      </h3>
+      <h4 className="truncate text-lg font-medium">오프라인</h4>
+    </div>
+  </BjListItem>
+));
+
+OfflineItem.displayName = "OfflineItem";
 
 function Sidebar() {
-  const { data, isError } = useFetch("favorites");
-
+  const { data, isError, isLoading, refetch } = useFetch("favorites");
   const [live, offline] = data ? splitAndSortFavorites(data.data) : [[], []];
 
+  if (isLoading) return <p>로딩 중...</p>;
+  if (isError) return <p>로그인 정보를 확인해주세요.</p>;
+  if (!data || data.data.length === 0) return <p>즐겨찾기한 BJ가 없습니다.</p>;
+
   return (
-    <nav aria-label="BJ 목록" tabIndex={0} className="px-4">
-      <h2 className="mb-2 text-lg font-bold">즐겨찾기</h2>
-      {isError ? (
-        <p>로그인 정보를 확인해주세요.</p>
-      ) : !data || data.data.length === 0 ? (
-        <p>즐겨찾기한 BJ가 없습니다.</p>
-      ) : (
-        <ul>
-          {live.length > 0 && <LiveBJList items={live} />}
-          {offline.length > 0 && <OfflineBJList items={offline} />}
-        </ul>
-      )}
-    </nav>
+    <aside
+      aria-label="BJ 목록"
+      className="scrollbar-thin scrollbar-rounded-full reset fixed h-[calc(100%-var(--gnb-height))] w-[inherit] overflow-auto px-2 pb-4 text-foreground"
+    >
+      <h2 className="mb-2 flex items-center text-lg font-bold">
+        즐겨찾기
+        <button
+          aria-label="새로고침"
+          className="ml-auto size-5"
+          onClick={refetch}
+        >
+          <Icons.refresh />
+        </button>
+      </h2>
+      <ul className="space-y-2">
+        <TooltipProvider>
+          {live.map((item) => (
+            <LiveItem key={item.user_id} item={item} />
+          ))}
+        </TooltipProvider>
+        {offline.map((item) => (
+          <OfflineItem key={item.user_id} item={item} />
+        ))}
+      </ul>
+    </aside>
   );
 }
 
